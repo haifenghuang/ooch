bbbb87ae23db82997084b1f06f086b2ba67f7e80
ooch <brettjnelson2@gmail.com>
2013-04-17 12:32:24 -0600

    Windows compile

---
+++ src/HelloWorld/build/HelloWorld_oo.d
@@ -0,0 +1,33 @@
+HelloWorld_oo.o ../build/HelloWorld_oo.d : HelloWorld_oo.c ../../../lib/ooch/ooc.h \
+ /usr/include/stdio.h /usr/include/features.h \
+ /usr/include/i386-linux-gnu/bits/predefs.h \
+ /usr/include/i386-linux-gnu/sys/cdefs.h \
+ /usr/include/i386-linux-gnu/bits/wordsize.h \
+ /usr/include/i386-linux-gnu/gnu/stubs.h \
+ /usr/include/i386-linux-gnu/gnu/stubs-32.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stddef.h \
+ /usr/include/i386-linux-gnu/bits/types.h \
+ /usr/include/i386-linux-gnu/bits/typesizes.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stdarg.h \
+ /usr/include/i386-linux-gnu/bits/stdio_lim.h \
+ /usr/include/i386-linux-gnu/bits/sys_errlist.h /usr/include/stdlib.h \
+ /usr/include/i386-linux-gnu/bits/waitflags.h \
+ /usr/include/i386-linux-gnu/bits/waitstatus.h /usr/include/endian.h \
+ /usr/include/i386-linux-gnu/bits/endian.h \
+ /usr/include/i386-linux-gnu/bits/byteswap.h \
+ /usr/include/i386-linux-gnu/sys/types.h /usr/include/time.h \
+ /usr/include/i386-linux-gnu/sys/select.h \
+ /usr/include/i386-linux-gnu/bits/select.h \
+ /usr/include/i386-linux-gnu/bits/sigset.h \
+ /usr/include/i386-linux-gnu/bits/time.h \
+ /usr/include/i386-linux-gnu/sys/sysmacros.h \
+ /usr/include/i386-linux-gnu/bits/pthreadtypes.h /usr/include/alloca.h \
+ ../../../config/project_settings.h ../../../lib/ooch/types_oo.h \
+ ../HelloWorld_oo.h ../../../lib/ooch/Executable_oo.h \
+ ../../../lib/ooch/inc/Executable_oocv.h \
+ ../../../lib/ooch/inc/Executable_ooiv.h \
+ ../../../lib/ooch/inc/Executable_ooimp.h \
+ ../../../lib/ooch/inc/Executable_oocmp.h ../src/HelloWorld_oocv.h \
+ ../src/HelloWorld_ooiv.h ../src/HelloWorld_ooimp.h \
+ ../../../lib/ooch/inc/Executable_oocmp.h ../src/HelloWorld_oocmp.h
