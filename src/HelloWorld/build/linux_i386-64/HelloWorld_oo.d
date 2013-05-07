6e01daef16f635641574a46b4adae51eb03512d5
ooch <brettjnelson2@gmail.com>
2013-05-07 13:18:10 -0600

    64 bit build

---
+++ src/HelloWorld/build/linux_i386-64/HelloWorld_oo.d
@@ -0,0 +1,33 @@
+HelloWorld_oo.o ../build/linux_i386-64/HelloWorld_oo.d : HelloWorld_oo.c ../../../lib/ooch/ooc.h \
+ /usr/include/stdio.h /usr/include/features.h \
+ /usr/include/x86_64-linux-gnu/bits/predefs.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.7/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.7/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/sigset.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h /usr/include/alloca.h \
+ ../../../config/project_settings.h ../../../lib/ooch/types_oo.h \
+ ../HelloWorld_oo.h ../../../lib/ooch/Executable_oo.h \
+ ../../../lib/ooch/inc/Executable_oocv.h \
+ ../../../lib/ooch/inc/Executable_ooiv.h \
+ ../../../lib/ooch/inc/Executable_ooimp.h \
+ ../../../lib/ooch/inc/Executable_oocmp.h ../src/HelloWorld_oocv.h \
+ ../src/HelloWorld_ooiv.h ../src/HelloWorld_ooimp.h \
+ ../../../lib/ooch/inc/Executable_oocmp.h ../src/HelloWorld_oocmp.h
